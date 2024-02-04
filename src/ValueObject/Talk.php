47475f1f6ae5d6f063fdf4ddaaf18ab4d29e49d4
Tomas Votruba <tomas.vot@gmail.com>
2024-02-04 12:52:02 +0100

    init classe

---
+++ src/ValueObject/Talk.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\ValueObject;
+
+final class Talk
+{
+    private $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
